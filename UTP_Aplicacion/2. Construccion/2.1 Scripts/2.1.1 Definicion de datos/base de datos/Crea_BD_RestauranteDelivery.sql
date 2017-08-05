9525ed3e7e02caa1bcd6f4631c34f8e199b56f0b
animedy <animedy12345@gmail.com>
2017-08-05 11:26:00 -0500

    Create Crea_BD_RestauranteDelivery.sql

---
+++ UTP_Aplicacion/2. Construccion/2.1 Scripts/2.1.1 Definicion de datos/base de datos/Crea_BD_RestauranteDelivery.sql
@@ -0,0 +1,13 @@
+-- --------------------------------------------------------
+-- Descripcion			: Script para creaciÃ³n de la base de datos de Restaurant	
+-- Fecha CreaciÃ³n		: 01/08/2017                                         		
+-- Fecha ModificaciÃ³n	: 															
+-- Autor					: Carlos Alberto Sanchez Aquino		
+-- VersiÃ³n				: Final										
+-- Cambios Importantes	:                                                         	
+--                                                                           		
+-- Propiedad de Pepe Tiburon                                                 		
+-- ---------------------------------------------------------
+
+DROP DATABASE IF EXISTS `restaurant`;
+CREATE DATABASE `restaurant` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
